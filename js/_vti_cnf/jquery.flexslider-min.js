7dbd241c5ecd2759314f4ce2589f6b1b30236ea4
Suriyaa Kudo <isc.suriyaa@gmail.com>
2014-07-26 12:55:11 +0200

    Create jquery.flexslider-min.js

---
+++ js/_vti_cnf/jquery.flexslider-min.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|11 Jul 2014 13:41:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|server.isc/contact.html server.isc/about.html server.isc/doc.html server.isc/features.html server.isc/index.html server.isc/developers.html
